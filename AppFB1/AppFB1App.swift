285c85cfa3a4bfbb21921d0e5173ae7ae8faa9f6
FredBdn <fredbdn@hotmail.fr>
2023-06-11 20:24:58 +0200

    Initial Commit

---
+++ AppFB1/AppFB1App.swift
@@ -0,0 +1,17 @@
+//
+//  AppFB1App.swift
+//  AppFB1
+//
+//  Created by FredBdn on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppFB1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
